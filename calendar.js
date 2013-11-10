6e0663872b491c3ea8b0715beb931c3457af65f8
Adrian Lee <adrianlee44@gmail.com>
2013-11-10 02:16:41 -0800

    Updated compiled files

--- calendar.js
+++ calendar.js
@@ -10,43 +10,39 @@
     };
 
     Calendar.prototype.template = {
-      calendar: "<div class=\"time\"></div><div class=\"events\"></div>",
+      events: "<div class=\"events\"></div>",
+      time: "<div class=\"time\"></div>",
       hour: "<div class=\"time-label\"/>",
-      event: "<div class=\"event\" />"
+      event: "<div class=\"event\" />",
+      currentTime: "<div class=\"current-time\"></div>"
     };
 
     function Calendar(el, options) {
-      var end, event, start, _i, _len, _ref;
       this.opts = $.extend({}, this.defaults, options);
       this.$el = $(el);
-      this.columnsY = [];
-      this.$el.html(this.template.calendar);
-      this.hourSection = this.$el.find(".time");
-      this.eventsSection = this.$el.find(".events");
+      this.hourSection = $(this.template.time);
+      this.eventsSection = $(this.template.events);
+      this.currentTimeBar = $(this.template.currentTime);
+      this.$el.empty().append(this.hourSection, this.eventsSection);
+      this.eventsSection.append(this.currentTimeBar);
+      this.$el.css({
+        width: this.opts.width
+      });
       this.events = this.opts.events.sort(function(a, b) {
         return a.start - b.start;
       });
-      this.startHour = 24;
-      this.endHour = 0;
-      _ref = this.events;
-      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-        event = _ref[_i];
-        if ((start = Math.floor(event.start / 100)) < this.startHour) {
-          this.startHour = start;
-        }
-        if ((end = Math.floor(event.end / 100)) > this.endHour) {
-          this.endHour = end;
-        }
-      }
+      this.startHour = 0;
+      this.endHour = 23;
       this.interval = this.opts.height / (this.endHour - this.startHour);
       this.processEvents();
       this.populateHours();
       this.populateEvents();
+      this.setCurrentTime();
     }
 
     Calendar.prototype.processEvents = function() {
-      var bottom, columnY, currentColY, end, event, height, i, index, length, start, top, _i, _len, _ref, _results;
-      this.columnsY = [
+      var bottom, columnY, columnsY, currentColY, end, event, height, i, index, length, start, top, _i, _len, _ref, _results;
+      columnsY = [
         {
           top: 0,
           bottom: 0
@@ -61,52 +57,42 @@
         top = this.interval * (start - this.startHour) - 20;
         height = this.interval * (end - start);
         bottom = top + height;
-        if (event.width == null) {
-          event.width = 0;
-        }
-        if (event.conflict == null) {
-          event.conflict = 1;
-        }
-        length = this.columnsY.length;
+        $.extend(event, {
+          top: top,
+          bottom: bottom,
+          height: height,
+          width: 0,
+          conflict: 1
+        });
+        length = columnsY.length;
         currentColY = {
           top: top,
           bottom: bottom,
-          index: index,
-          conflict: []
+          index: index
         };
         _results.push((function() {
-          var _j, _len1, _ref1, _results1;
-          _ref1 = this.columnsY;
+          var _j, _len1, _results1;
           _results1 = [];
-          for (i = _j = 0, _len1 = _ref1.length; _j < _len1; i = ++_j) {
-            columnY = _ref1[i];
+          for (i = _j = 0, _len1 = columnsY.length; _j < _len1; i = ++_j) {
+            columnY = columnsY[i];
             if (top >= columnY.bottom) {
-              this.columnsY[i] = currentColY;
+              columnsY[i] = currentColY;
               if (event.column == null) {
                 event.column = i;
               }
-              event.width++;
-              event.top = top;
-              event.bottom = bottom;
-              _results1.push(event.height = height);
+              _results1.push(event.width++);
             } else if (event.width > 0) {
               event.conflict = length;
               break;
-            } else if (length - 1 === i) {
-              currentColY.conflict.push(columnY.index);
-              event.column = this.columnsY.push(currentColY) - 1;
-              columnY.conflict.push(index);
-              event.width++;
-              event.conflict++;
-              event.top = top;
-              event.bottom = bottom;
-              event.height = height;
-              this.events[columnY.index].conflict++;
-              break;
             } else {
+              event.conflict++;
               this.events[columnY.index].conflict++;
-              currentColY.conflict.push(columnY.index);
-              _results1.push(event.conflict++);
+              if (length - 1 === i) {
+                event.column = columnsY.push(currentColY) - 1;
+                _results1.push(event.width++);
+              } else {
+                _results1.push(void 0);
+              }
             }
           }
           return _results1;
@@ -116,68 +102,48 @@
     };
 
     Calendar.prototype.populateHours = function() {
-      var count, current, hour, period, time, _i, _ref, _ref1, _results;
-      count = 0;
+      var hour, period, time, _i, _ref, _ref1, _results;
       _results = [];
       for (hour = _i = _ref = this.startHour, _ref1 = this.endHour; _ref <= _ref1 ? _i <= _ref1 : _i >= _ref1; hour = _ref <= _ref1 ? ++_i : --_i) {
-        current = $(this.template.hour);
         time = hour === 12 ? hour : hour % 12;
         period = hour < 12 ? "AM" : "PM";
-        current.text("" + time + " " + period);
-        current.css({
-          top: count * this.interval
-        });
-        this.hourSection.append(current);
-        _results.push(count++);
+        _results.push($(this.template.hour).text("" + time + " " + period).css({
+          top: (hour - this.startHour) * this.interval
+        }).appendTo(this.hourSection));
       }
       return _results;
     };
 
     Calendar.prototype.populateEvents = function() {
-      var event, eventEle, hasConflict, width, _i, _len, _ref, _results;
-      hasConflict = function(current, target) {
-        var _ref, _ref1;
-        return (target.start < (_ref = current.start) && _ref < target.end) || (target.start < (_ref1 = current.end) && _ref1 < target.end);
-      };
+      var event, eventEle, width, _i, _len, _ref, _results;
       _ref = this.events;
       _results = [];
       for (_i = 0, _len = _ref.length; _i < _len; _i++) {
         event = _ref[_i];
-        eventEle = $(this.template.event);
-        eventEle.text("Event " + event.id);
-        width = (this.opts.width * 0.85 - 20) * (event.width / event.conflict) - 2;
-        eventEle.css({
+        width = (event.width / event.conflict) * 100;
+        _results.push(eventEle = $(this.template.event).text("Event " + event.id).css({
           top: event.top,
           height: event.height,
-          width: width,
-          left: width * event.column + 10
-        });
-        _results.push(this.eventsSection.append(eventEle));
+          width: "" + width + "%",
+          left: "" + (width * event.column) + "%"
+        }).appendTo(this.eventsSection));
       }
       return _results;
     };
 
-    Calendar.prototype.findAvailableCol = function(columns, top) {
-      var columnY, i, _i, _len, _ref;
-      if (columns == null) {
-        columns = 1;
-      }
-      if (columns === 1) {
-        return {
-          column: columns,
+    Calendar.prototype.setCurrentTime = function() {
+      var interval, updatePosition,
+        _this = this;
+      interval = 60 / this.interval * 1000;
+      (updatePosition = function() {
+        var now, top;
+        now = new Date();
+        top = _this.interval * (now.getHours() - _this.startHour + now.getMinutes() / 60);
+        return _this.currentTimeBar.css({
           top: top
-        };
-      }
-      _ref = this.columnsY;
-      for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
-        columnY = _ref[i];
-        if (columnY <= top) {
-          return {
-            column: i + 1,
-            top: top
-          };
-        }
-      }
+        });
+      })();
+      return setInterval(updatePosition, interval);
     };
 
     return Calendar;
