b1d67a7ce2521155d02b0484e2ef8a8357713584
Adrian Lee <adrianlee44@gmail.com>
2013-11-20 01:08:33 -0800

    Update compiled file

--- calendar.js
+++ calendar.js
@@ -1,12 +1,24 @@
-(function() {
-  var Calendar,
-    __slice = [].slice;
+/*
+@name Calendar
+@description
+Just a small jQuery plugin to render a daily agenda
 
+@author Adrian Lee
+@email adrian@adrianlee.me
+@license MIT
+*/
+
+var __slice = [].slice;
+
+(function(window, $) {
+  var Calendar;
   Calendar = (function() {
     Calendar.prototype.defaults = {
       events: [],
       height: 800,
-      width: 600
+      width: 600,
+      startHour: 0,
+      endHour: 24
     };
 
     Calendar.prototype.template = {
@@ -18,30 +30,50 @@
     };
 
     function Calendar(el, options) {
+      var currentTimeBar, eventsSection, hourSection;
       this.opts = $.extend({}, this.defaults, options);
       this.$el = $(el);
-      this.hourSection = $(this.template.time);
-      this.eventsSection = $(this.template.events);
-      this.currentTimeBar = $(this.template.currentTime);
-      this.$el.empty().append(this.hourSection, this.eventsSection);
-      this.eventsSection.append(this.currentTimeBar);
+      hourSection = $(this.template.time);
+      currentTimeBar = $(this.template.currentTime);
+      eventsSection = $(this.template.events).append(currentTimeBar);
+      this.$el.empty().append(hourSection, eventsSection);
       this.$el.css({
         width: this.opts.width
       });
-      this.events = this.opts.events.sort(function(a, b) {
-        return a.start - b.start;
-      });
-      this.startHour = 0;
-      this.endHour = 23;
-      this.interval = this.opts.height / (this.endHour - this.startHour);
-      this.processEvents();
-      this.populateHours();
-      this.populateEvents();
-      this.setCurrentTime();
+      this.interval = this.opts.height / (this.opts.endHour - this.opts.startHour);
+      this.processEvents(this.opts.events);
+      this.populateHours(hourSection);
+      this.populateEvents(eventsSection);
+      this.setCurrentTime(currentTimeBar);
     }
 
-    Calendar.prototype.processEvents = function() {
+    /*
+    @name normalizeTime
+    @type function
+    @description
+    To convert start and end time to format used by calendar plugin
+    @param {Object} event Event object with start and end time
+    @returns {Object} Updated event with new start and end time
+    */
+
+
+    Calendar.prototype.normalizeTime = function(event) {
+      var end, start;
+      start = event.start;
+      end = event.end;
+      if (typeof start === "string" && start.indexOf("T") > -1) {
+        event.start = parseInt(start.split("T")[1].substring(0, 4));
+      }
+      if (typeof end === "string" && end.indexOf("T") > -1) {
+        return event.end = parseInt(end.split("T")[1].substring(0, 4));
+      }
+    };
+
+    Calendar.prototype.processEvents = function(events) {
       var bottom, columnY, columnsY, currentColY, end, event, height, i, index, length, start, top, _i, _len, _ref, _results;
+      this.events = events.sort(function(a, b) {
+        return a.start - b.start;
+      });
       columnsY = [
         {
           top: 0,
@@ -52,9 +84,10 @@
       _results = [];
       for (index = _i = 0, _len = _ref.length; _i < _len; index = ++_i) {
         event = _ref[index];
+        this.normalizeTime(event);
         start = Math.floor(event.start / 100) + ((event.start % 100) / 60);
         end = Math.floor(event.end / 100) + ((event.end % 100) / 60);
-        top = this.interval * (start - this.startHour) - 20;
+        top = this.interval * (start - this.opts.startHour) - 20;
         height = this.interval * (end - start);
         bottom = top + height;
         $.extend(event, {
@@ -101,20 +134,38 @@
       return _results;
     };
 
-    Calendar.prototype.populateHours = function() {
+    /*
+    @name populateHours
+    @type function
+    @description
+    Create the side hour section
+    @param {DOM element} element Element to populate hour labels
+    */
+
+
+    Calendar.prototype.populateHours = function(element) {
       var hour, period, time, _i, _ref, _ref1, _results;
       _results = [];
-      for (hour = _i = _ref = this.startHour, _ref1 = this.endHour; _ref <= _ref1 ? _i <= _ref1 : _i >= _ref1; hour = _ref <= _ref1 ? ++_i : --_i) {
-        time = hour === 12 ? hour : hour % 12;
+      for (hour = _i = _ref = this.opts.startHour, _ref1 = this.opts.endHour - 1; _ref <= _ref1 ? _i <= _ref1 : _i >= _ref1; hour = _ref <= _ref1 ? ++_i : --_i) {
         period = hour < 12 ? "AM" : "PM";
+        time = hour % 12 ? hour % 12 : 12;
         _results.push($(this.template.hour).text("" + time + " " + period).css({
-          top: (hour - this.startHour) * this.interval
-        }).appendTo(this.hourSection));
+          top: (hour - this.opts.startHour) * this.interval
+        }).appendTo(element));
       }
       return _results;
     };
 
-    Calendar.prototype.populateEvents = function() {
+    /*
+    @name populateEvents
+    @type function
+    @description
+    Create and position all event boxes
+    @param {DOM element} element Element to populate event boxes
+    */
+
+
+    Calendar.prototype.populateEvents = function(element) {
       var event, eventEle, width, _i, _len, _ref, _results;
       _ref = this.events;
       _results = [];
@@ -126,20 +177,29 @@
           height: event.height,
           width: "" + width + "%",
           left: "" + (width * event.column) + "%"
-        }).appendTo(this.eventsSection));
+        }).appendTo(element));
       }
       return _results;
     };
 
-    Calendar.prototype.setCurrentTime = function() {
+    /*
+    @name setCurrentTime
+    @type function
+    @description
+    Update the position of the current time line
+    @param {DOM element} element Current time line
+    */
+
+
+    Calendar.prototype.setCurrentTime = function(element) {
       var interval, updatePosition,
         _this = this;
       interval = 60 / this.interval * 1000;
       (updatePosition = function() {
         var now, top;
         now = new Date();
-        top = _this.interval * (now.getHours() - _this.startHour + now.getMinutes() / 60);
-        return _this.currentTimeBar.css({
+        top = _this.interval * (now.getHours() - _this.opts.startHour + now.getMinutes() / 60);
+        return element.css({
           top: top
         });
       })();
@@ -149,8 +209,7 @@
     return Calendar;
 
   })();
-
-  $.fn.extend({
+  return $.fn.extend({
     calendar: function() {
       var args, options;
       options = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
@@ -162,10 +221,9 @@
           $this.data("calendar", (data = new Calendar(this, options)));
         }
         if (typeof option === "string") {
-          return data[option].apply(data, argsW);
+          return data[option].apply(data, args);
         }
       });
     }
   });
-
-}).call(this);
+})(window, jQuery);
